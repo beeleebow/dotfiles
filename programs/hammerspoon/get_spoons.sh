2dcde24476e45791fe3e9e3a4d7875f7a0e625a3
Brad Bow <brad.bow@stacktracehq.com>
2019-03-04 11:08:48 +1000

    Setup hammerspoon

---
+++ programs/hammerspoon/get_spoons.sh
@@ -0,0 +1,7 @@
+SPOON_DIR=$HOME/.hammerspoon/Spoons
+
+# allows two mode escape on single key
+git clone https://github.com/jasonrudolph/ControlEscape.spoon $SPOON_DIR/ControlEscape.spoon
+sh $SPOON_DIR/ControlEscape.spoon/script/setup
+
+
