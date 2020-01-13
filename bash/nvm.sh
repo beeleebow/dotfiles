831d041733f0b0e7a11b385b388c235ba65c4eee
Brad Bow <brad.bow@stacktracehq.com>
2020-01-14 09:00:10 +1000

    Stops initialisation of nvm inside a nix-shell

--- bash/nvm.sh
+++ bash/nvm.sh
@@ -1,3 +1,9 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# don't run nvm if we're in the nix shell
+if [ ! -z $IN_NIX_SHELL ]; then
+  return
+else
+  printf "Initialising nvm (won't do this inside a nix shell)\n"
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
