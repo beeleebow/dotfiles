5943407456290dd173f40a2d8a89b42a810fb523
Brad Bow <brad.bow@stacktracehq.com>
2020-09-10 18:14:13 +1000

    Add dnb alias for dotnet build

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,4 +2,5 @@ alias refresh='fresh && source ~/.bash_profile'
 alias g='git'
 alias fmar='git fetch origin && git rebase origin/master'
 alias ns='nix-shell --command "fresh && source ~/.bash_profile; return"'
+alias dnb='dotnet build'
 
