1ce356fb3ec1d5ea4396380b9b5b98eef27ae1c0
Brad Bow <brad.bow@stacktracehq.com>
2020-10-28 10:45:27 +1000

    Add aliases for generating ulids

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -3,4 +3,6 @@ alias g='git'
 alias fmar='git fetch origin && git rebase origin/master'
 alias ns='nix-shell --command "fresh && source ~/.bash_profile; return"'
 alias dnb='dotnet build'
+alias ulid='$HOME/src/util/ulid/run.py && echo ""'
+alias ulidc='$HOME/src/util/ulid/run.py | pbcopy'
 
