697e4c8c108fb125214485030f417f44c1c89c0b
Brad Bow <bradbow@fastmail.com>
2021-10-09 10:14:26 +1000

    alias: add awip alias for adding all changes and folding into previous commit

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -5,4 +5,5 @@ alias ns='nix-shell --command "fresh && source ~/.bash_profile; return"'
 alias dnb='dotnet build'
 alias ulid='$HOME/src/util/ulid/run.py && echo ""'
 alias ulidc='$HOME/src/util/ulid/run.py | pbcopy'
+alias awip="git add --all && git commit -S --amend --no-edit"
 
