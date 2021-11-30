3429d28f134764fa81874764b9ab82ab7c12b778
Brad Bow <bradbow@fastmail.com>
2021-11-30 15:17:40 +1000

    add dev scripts to path

--- bash/path.sh
+++ bash/path.sh
@@ -1,1 +1,1 @@
-export PATH=$PATH:/usr/local/bin:$HOME/.local/bin:$HOME/.nix-profile/bin:$HOME/.cargo/bin
+export PATH=$PATH:$HOME/src/ftr-dev-scripts/commands:/usr/local/bin:$HOME/.local/bin:$HOME/.nix-profile/bin:$HOME/.cargo/bin
