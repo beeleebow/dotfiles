2735fc97c62a53cdf5bddf519f1f66580dfb7ae7
Brad Bow <bradbow@fastmail.com>
2024-09-11 09:11:38 +1000

    sc alias for summarising commits in reverse

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,5 +7,5 @@ alias ulid='$HOME/src/util/ulid/run.py && echo ""'
 alias ulidc='$HOME/src/util/ulid/run.py | pbcopy'
 alias awip="git add --all && git commit -S --amend --no-edit"
 alias gap="git add -p"
-alias sc="git log --pretty=format:'## %h - %s%n%b'"
+alias sc="git log --reverse --pretty=format:'## %h - %s%n%b'"
 
